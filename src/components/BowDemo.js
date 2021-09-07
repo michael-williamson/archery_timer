9f4c0962eee61f5d620ccdd7afa1a715609d26f4
michael-williamson <michaeldavid1721@yahoo.com>
2021-09-07 17:33:22 -0500

    added form inputs

--- src/components/BowDemo.js
+++ src/components/BowDemo.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Arrow } from "./Arrow";
-import { Grid, IconButton, Typography } from "@material-ui/core";
-import { TrackChanges, ArrowRightAlt } from "@material-ui/icons";
+import { Grid, IconButton, Typography, Input } from "@material-ui/core";
+import { TrackChanges } from "@material-ui/icons";
 import bowDrawAudioURL from "../media/bow_draw.mp3";
 import arrowImpactURL from "../media/arrow_impact.mp3";
 
@@ -10,34 +10,57 @@ export const BowDemo = () => {
   const [showCounter, setShowCounter] = useState(false);
   const [counter, setCounter] = useState(3);
   const [animationPlayState, setAnimationPlayState] = useState("paused");
+  const [feetPerSecond, setFeetPerSecond] = useState(170);
+  const [timeTilTarget, setTimeTilTarget] = useState(120);
+  const [distanceTilTarget, setDistanceTilTarget] = useState(210);
 
   useEffect(() => {
-    !counter && setAnimationPlayState("running");
+    const arrowImpactAudio = new Audio(arrowImpactURL);
+    if (!counter) {
+      setAnimationPlayState("running");
+      setTimeout(() => {
+        arrowImpactAudio.play();
+      }, timeTilTarget);
+    }
     counter > 0 &&
       showCounter &&
       setTimeout(() => setCounter(counter - 1), 1000) &&
       setDrawing("Releasing");
-
-    console.log(new Date());
-    console.log(counter, "counter in useEffect");
   });
 
+  const handleChange = (prop) => (event) => {
+    const timeTilTargetCalculator = (fps, distance) => {
+      console.log(distance, "distance in yards");
+      let val = (distance * 3) / fps;
+      val = val * 1000;
+      val = val.toFixed();
+      setTimeTilTarget(val);
+    };
+
+    console.log(prop, "prop in console");
+
+    "fps" === prop
+      ? timeTilTargetCalculator(event.target.value, distanceTilTarget)
+      : timeTilTargetCalculator(feetPerSecond, event.target.value);
+    "fps" === prop
+      ? setFeetPerSecond(event.target.value)
+      : setDistanceTilTarget(event.target.value);
+  };
+
   const archeryDrawSequence = () => {
+    // created audio objects for each sound
     const bowDrawAudio = new Audio(bowDrawAudioURL);
-    const arrowImpactAudio = new Audio(arrowImpactURL);
+    //play drawing of the bow
     bowDrawAudio.play();
+    //begin sequence of steps with timers
     setDrawing("Drawing Arrow");
     setTimeout(() => {
       setDrawing("Arrow Drawn");
     }, 3000);
-
+    // begins the countdown in useEffect Hook by setting showCounter to true
     setTimeout(() => {
       setShowCounter(true);
     }, 5000);
-
-    setTimeout(() => {
-      arrowImpactAudio.play();
-    }, 9000);
   };
   return (
     <Grid container justifyContent="center">
@@ -47,10 +70,16 @@ export const BowDemo = () => {
         </IconButton>
       </Grid>
       <Grid item xs={12}>
+        <Input value={feetPerSecond} onChange={handleChange("fps")} />
+      </Grid>
+      <Grid item xs={12}>
+        <Input value={distanceTilTarget} onChange={handleChange("yards")} />
+      </Grid>
+      <Grid item xs={12}>
         <Typography variant="h3" component="h3">
           {drawing}
         </Typography>
-        {counter > -1 && showCounter === true ? (
+        {counter > -1 && showCounter ? (
           <Typography variant="h3" component="h3">
             {counter}
           </Typography>
@@ -61,7 +90,10 @@ export const BowDemo = () => {
         )}
       </Grid>
       <Grid container item xs={12}>
-        <Arrow animationPlayState={animationPlayState} />
+        <Arrow
+          animationPlayState={animationPlayState}
+          timeTilTarget={timeTilTarget}
+        />
       </Grid>
     </Grid>
   );
